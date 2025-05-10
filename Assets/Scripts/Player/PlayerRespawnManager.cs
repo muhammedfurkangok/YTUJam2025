b47936fcd20c29ec6e9ead131aec0406ab404650
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-05-10 16:22:29 +0300

    Player Effect System Progress

--- Assets/Scripts/Player/PlayerRespawnManager.cs
+++ Assets/Scripts/Player/PlayerRespawnManager.cs
@@ -4,6 +4,21 @@ namespace Player
 {
     public class PlayerRespawnManager : MonoBehaviour
     {
+
+        public void OnDie()
+        {
+            //die anim
+        }
         
+        public void GetVaccinated()
+        {
+            //play animation
+        }
+        public void OnRespawn()
+        {
+            GetVaccinated();
+            //card;
+            //card equal thing happened.
+        }
     }
 }
