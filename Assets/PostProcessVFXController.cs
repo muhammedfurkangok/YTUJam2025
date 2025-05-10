88153fa9274f155cada1391c1338c0ff1604ccf0
Agah <agahgames260@gmail.com>
2025-05-11 02:41:31 +0300

    working postprocess effectss yayyy! made the cutscene

--- Assets/PostProcessVFXController.cs
+++ Assets/PostProcessVFXController.cs
@@ -1,40 +1,83 @@
 using DG.Tweening;
-using DG.Tweening.Core;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using Utilities;
+using static Unity.Collections.AllocatorManager;
 
 public class PostProcessVFXController : MonoBehaviour
 {
-    Volume volume;
+    private Volume volume;
+
+    // Cached default values
+    private float defaultBloomIntensity;
+    private float defaultBloomScatter;
+    private float defaultBloomThreshold;
+    private float defaultChromaticAberration;
 
     private void Start()
     {
         volume = GetComponent<Volume>();
+
+        // Cache default Bloom values
+        if (volume.sharedProfile.TryGet(out Bloom bloom))
+        {
+            defaultBloomIntensity = bloom.intensity.value;
+            defaultBloomScatter = bloom.scatter.value;
+            defaultBloomThreshold = bloom.threshold.value;
+        }
+
+        // Cache default Chromatic Aberration
+        if (volume.sharedProfile.TryGet(out ChromaticAberration ca))
+        {
+            defaultChromaticAberration = ca.intensity.value;
+        }
+
+        // Subscribe to events
         DoctorController.OnDoctorStab += DoctorController_OnDoctorStab;
         DoctorController.OnDoctorEnject += DoctorController_OnDoctorEnject;
         DoctorController.OnDoctorAttackEnd += DoctorController_OnDoctorAttackEnd;
     }
 
-    private void DoctorController_OnDoctorAttackEnd(float obj)
+    private void DoctorController_OnDoctorStab(float duration)
     {
-        var b = volume.sharedProfile.TryGet<Bloom>(out var component);
-        DOTween.To(() => component.intensity.value, x => component.intensity.value = x, 1f, obj);
-        component.scatter.Interp(0.4f, to: 1f, obj);
-        component.threshold.Interp(0.75f, to: 0f, obj);
+        volume.DoAberrate(duration); 
+    }
+
+    private void DoctorController_OnDoctorEnject(float duration)
+    {
+        if (!volume.sharedProfile.TryGet(out ChromaticAberration ca)) return;
+
+        DOTween.To(() => ca.intensity.value, x => ca.intensity.value = x, 1f, duration);
 
     }
 
-    private void DoctorController_OnDoctorEnject(float obj)
+    private void DoctorController_OnDoctorAttackEnd(float duration)
     {
-        var b = volume.sharedProfile.TryGet<ChromaticAberration>(out var component);
-        if (!b) return;
-        DOTween.To(() => component.intensity.value, x => component.intensity.value = x, 1f, obj);
+        if (!volume.sharedProfile.TryGet(out Bloom bloom)) return;
+
+        // Animate to strong bloom effect
+        DOTween.To(() => bloom.intensity.value, x => bloom.intensity.value = x, 100f, duration*4f);
+        DOTween.To(() => bloom.scatter.value, x => bloom.scatter.value = x, 1f, duration);
+        DOTween.To(() => bloom.threshold.value, x => bloom.threshold.value = x, 0f, duration);
     }
 
-    private void DoctorController_OnDoctorStab(float obj)
+    private void ResetToNormal()
     {
-        volume.DoAberrate(obj);
+        if (!volume.sharedProfile.TryGet(out Bloom bloom)) return;
+
+        bloom.intensity.value = defaultBloomIntensity;
+        bloom.scatter.value = defaultBloomScatter;
+        bloom.threshold.value = defaultBloomThreshold;
+
+        if (!volume.sharedProfile.TryGet(out ChromaticAberration ca)) return;
+
+        ca.intensity.value = defaultChromaticAberration;
+
+    }
+
+    private void OnDisable()
+    {
+        ResetToNormal();
     }
 }
